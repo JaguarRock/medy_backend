5be4d2f2db4847d95ddbe89dea0e8a524d6747cf
InChul <49394875+inchori@users.noreply.github.com>
2020-05-05 14:48:45 +0900

    first commit

---
+++ models/medicineBag.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let medicineBagSchema = new Schema({
+    id : {
+        type: Number
+    },
+    bagName : {
+        type: String
+    },
+    bagConsist: {
+        type: String
+    }
+}, {
+    collection: 'medicineBag'
+})
+
+module.exports = mongoose.model('Student', medicineBagSchema);
